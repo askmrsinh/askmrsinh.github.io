4045f9015d2ff062d083a721e60c629f403a3c8a
Ashesh Singh <ask@mrsinh.com>
2025-08-18 01:28:47 +0100

    Add Commit Hash to Global Variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import * as child from 'child_process';
+
+const commitHash = child.execSync('git rev-parse --short HEAD').toString();
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    define: {
+      __COMMIT_HASH__: JSON.stringify(commitHash)
+    }
+  },
   integrations: [mdx()]
 });
